3827ce3be183395406fe7e0d6e841777a699d586
Lulu3750 <113137715+Lulu3750@users.noreply.github.com>
2022-09-08 14:02:56 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,13 @@
+input.onGesture(Gesture.TiltLeft, function () {
+	
+})
+radio.onReceivedString(function (receivedString) {
+	
+})
+basic.showIcon(IconNames.Happy)
+basic.showIcon(IconNames.Silly)
+music.startMelody(music.builtInMelody(Melodies.Funk), MelodyOptions.ForeverInBackground)
+basic.showIcon(IconNames.Surprised)
+basic.forever(function () {
+	
+})
